dc93ab740b7eea5176e62c0ee72885c4c03fad7d
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-24 23:44:15 +0000

    edd cart qty id

--- template-parts/nav/logged_in_nav.php
+++ template-parts/nav/logged_in_nav.php
@@ -1,6 +1,6 @@
 <div class="dap_top_nav w-clearfix">
   <div class="dap_960">
-    <a href="<?php echo edd_get_checkout_uri(); ?>">
+    <a class="dap_top_link" id="dap_cart" href="<?php echo edd_get_checkout_uri(); ?>">
     	Cart (<span class="header-cart edd-cart-quantity"><?php echo edd_get_cart_quantity(); ?></span>)
     </a>
       

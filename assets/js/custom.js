8d98c1ce2e4658b1f57c30fbfd4ba9f9d02ed683
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-25 00:00:34 +0000

    cart add jquery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,6 +3,8 @@ $(function(){
    
    add_to_nav();
    
+   get_cart();
+   
 });
 
 
@@ -16,3 +18,24 @@ function add_to_nav(){
     $(find_sub).last().addClass("dap_join");
     console.log('adding menu class');
 }
+
+function add_cart_to_nav(){
+    var get_cart = $('.dap_top_link');
+
+    
+    $(get_cart).first().prepend("hello worlds");
+    console.log('getting cart');
+}
+
+// function appendText() {
+//     var txt1 = "<a class="dap_top_link dap_cart" href="<?php echo edd_get_checkout_uri(); ?>Cart (<span class="header-cart edd-cart-quantity"><?php echo edd_get_cart_quantity(); ?></span>)
+//     </a>";"               // Create element with HTML  
+//     var txt2 = $("<p></p>").text("Text.");   // Create with jQuery
+//     var txt3 = document.createElement("p");  // Create with DOM
+//     txt3.innerHTML = "Text.";
+//     $("body").append(txt1, txt2, txt3);      // Append the new elements 
+// }
+
+//   <a class="dap_top_link dap_cart" href="<?php echo edd_get_checkout_uri(); ?>">
+//     	Cart (<span class="header-cart edd-cart-quantity"><?php echo edd_get_cart_quantity(); ?></span>)
+//     </a>

cec53e961f9e323b875e2d50474bda3c04be62fe
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-26 04:13:31 +0000

    add edd menu via php

--- template-parts/nav/nav_in.php
+++ template-parts/nav/nav_in.php
@@ -1,4 +1,20 @@
 <?php
+  
+  function wdw_nav_wrap() {
+    $new_nav_wrap  = '<ul id="%1$s" class="%2$s">';
+    $new_nav_wrap .= '%3$s';
+    $new_nav_wrap .= '<li class="menu-item">';
+    $new_nav_wrap .= '<a href="' . edd_get_checkout_uri() . '">';
+    $new_nav_wrap .= 'Cart (<span class="edd-cart-quantity">' . edd_get_cart_quantity() .'</span>)';
+    $new_nav_wrap .= '</a>';
+    $new_nav_wrap .= '</li>';
+    $new_nav_wrap .= '</ul>';
+
+    return $new_nav_wrap;
+    
+}
+
+
   $defaults = array(
       'theme_location'  => 'logged-in',
       'menu'            => '',
@@ -13,7 +29,7 @@
       'after'           => '',
       'link_before'     => '',
       'link_after'      => '',
-      'items_wrap'      => '<nav id="%1$s" class="%2$s">%3$s </nav>',
+      'items_wrap'      =>  wdw_nav_wrap() & '<nav id="%1$s" class="%2$s">%3$s </nav>',
       'depth'           => 0,
       'walker'          => ''
   );
@@ -22,4 +38,5 @@
   $replace = array('', '<a class="dap_top_link"');
   
   echo str_replace($find, $replace, wp_nav_menu( $defaults ));
+
 ?>   

63ab87bce48ea078b0a14153a1470e6fb49547d3
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-27 00:29:02 +0000

    sales cpt single updated

--- single-sales.php
+++ single-sales.php
@@ -18,14 +18,21 @@ get_header();
 
           <div class="w-row">
             <div class="w-col w-col-8">
+               <?php if (get_field('sales_video')) :?>
+
               <div style="padding-top:56.17021276595745%" class="w-video w-embed">
                   <!--<iframe class="embedly-embed" src="https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FEOQ2hMEKrsM%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEOQ2hMEKrsM&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FEOQ2hMEKrsM%2Fhqdefault.jpg&key=96f1f04c5f4143bcb0f2e68c87d65feb&type=text%2Fhtml&schema=youtube" scrolling="no" frameborder="0" allowfullscreen=""></iframe>-->
-                        <?php if (get_field('sales_video')) :?>
                             <div class="embed-container">
                                 <?php the_field('sales_video'); ?> 
                             </div>
-                        <?php endif; ?>
+                      
               </div>
+               <?php endif; ?>
+               <?php if (get_field('sales_video')) :?>
+                <img class="dap_screencast_salesimage">
+                     <?php the_field('sales_image'); ?> 
+                </img>
+                 <?php endif; ?>
               <h2>Description</h2>
               <p>
                 <?php if (get_field('sales_description')) :?>
@@ -34,30 +41,40 @@ get_header();
                             </div>
                 <?php endif; ?>                         
               </p>
-              <h2>Topic Covered</h2>
-              <ul class="dap_screencast_ul">
-                <?php if (get_field('topics_covered')) :?>
-                            <div class="embed-container">
-                                <?php the_field('topics_covered'); ?> 
-                            </div>
-                <?php endif; ?>                         
-              </ul>
+             
             </div>
             <div class="w-col w-col-4">
-              <h1 class="dap_screencast_price">
-                 <?php if (get_field('sales_price')) :?>
+              <div class="dap_screencast_purchase">
+                    <h3 class="dap_centered_heading">Purchase Screencast</h3>
+                     <?php if (get_field('sales_shortcode')) :?>
+                        <div class="embed-container">
+                            <?php the_field('sales_shortcode'); ?> 
+                        </div>
+                    <?php endif; ?>                                  
+                    <div class="dap_sale_text">
+                         <?php if (get_field('sales_disclaimer')) :?>
                             <div class="embed-container">
-                                <?php the_field('sales_price'); ?> 
+                                <?php the_field('sales_disclaimer'); ?> 
                             </div>
-                <?php endif; ?>                     
+                        <?php endif; ?>                          
+                    </div>
+                  </div>                
+                        
+                
+              <!--<h1 class="dap_screencast_price">-->
+              <!--   <!?php if (get_field('sales_price')) :?>-->
+              <!--              <div class="embed-container">-->
+              <!--                  <!?php the_field('sales_price'); ?> -->
+              <!--              </div>-->
+              <!--  <!?php endif; ?>                     -->
                   <!--<span class="dap_crossed_out_price">$80</span> $60-->
-              </h1>
+              <!--</h1>-->
               <!--<div class="dap_sale_text">This is some text inside of a div block.</div>-->
-               <?php if (get_field('sales_buy')) :?>
-                            <div class="embed-container">
-                                <?php the_field('sales_buy'); ?> 
-                            </div>
-                <?php endif; ?>   
+               <!--<!?php if (get_field('sales_buy')) :?>-->
+               <!--             <div class="embed-container">-->
+               <!--                 <!?php the_field('sales_buy'); ?> -->
+               <!--             </div>-->
+               <!-- <!?php endif; ?>   -->
           </div>
         
        <?php endwhile; ?>

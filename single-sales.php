af71993bbb94cbe5704f186862a40f634e030beb
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-27 00:44:52 +0000

    remove embed-containers

--- single-sales.php
+++ single-sales.php
@@ -22,10 +22,9 @@ get_header();
         
                       <div style="padding-top:56.17021276595745%" class="w-video w-embed">
                           <!--<iframe class="embedly-embed" src="https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FEOQ2hMEKrsM%3Ffeature%3Doembed&url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEOQ2hMEKrsM&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FEOQ2hMEKrsM%2Fhqdefault.jpg&key=96f1f04c5f4143bcb0f2e68c87d65feb&type=text%2Fhtml&schema=youtube" scrolling="no" frameborder="0" allowfullscreen=""></iframe>-->
-                                    <div class="embed-container">
-                                        <?php the_field('sales_video'); ?> 
-                                    </div>
-                              
+                            <div class="embed-container">
+                                <?php the_field('sales_video'); ?> 
+                            </div>
                       </div>
                        <?php endif; ?>
                        <?php if (get_field('sales_video')) :?>
@@ -42,32 +41,28 @@ get_header();
                         <?php endif; ?>                         
                       </p>
                      
-            </div>
+                    </div>
                     <div class="w-col w-col-4">
                         <div class="dap_screencast_purchase">
                             
                                 <h3 class="dap_centered_heading">Purchase Screencast</h3>
                                 
                                  <?php if (get_field('sales_shortcode')) :?>
-                                    <div class="embed-container">
-                                        <?php the_field('sales_shortcode'); ?> 
-                                    </div>
+                                    <?php the_field('sales_shortcode'); ?> 
                                 <?php endif; ?>  
                                 
                                  <?php if (get_field('sales_disclaimer')) :?>
-                                    <div class="embed-container">
-                                        <p class="dap_sale_text"><?php the_field('sales_disclaimer'); ?> </p>
-                                    </div>
+                                    <p class="dap_sale_text"><?php the_field('sales_disclaimer'); ?> </p>
                                 <?php endif; ?>                          
                                 
-                      </div>                
+                          </div>                
+                    </div>
+        
+             <?php endwhile; ?>
+        <?php endif; ?>  
+        
                 </div>
-        
-       <?php endwhile; ?>
-    <?php endif; ?>  
-        
     </div>
-</div>
 </div>
 
 <?php get_footer(); ?>

caa3d792dc0f5988490b44ddef5129c3d41bb209
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-27 22:22:02 +0000

    searchform.php svg added back

--- searchform.php
+++ searchform.php
@@ -13,5 +13,5 @@
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'devandplay' ); ?></span>
 	</label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'devandplay' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'devandplay' ); ?></span></button>
+	<button type="submit" class="search-submit"><?php echo devandplay_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'devandplay' ); ?></span></button>
 </form>

58a084c9ddcd10b2eedf460aff2e871812cdfb36
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-02 20:45:43 +0000

    gamefeelideas.css

--- functions.php
+++ functions.php
@@ -1,31 +1,31 @@
 <?php
 //Loading in Scripts
-function devandplay_enqueue_styles(){
+function gamefeelideas_enqueue_styles(){
     wp_enqueue_script('webflow-js', get_template_directory_uri() . '/assets/js/webflow.js', array('jquery'));
-     wp_enqueue_script('devandplay_custom', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'));
+     wp_enqueue_script('gamefeelideas_custom', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'));
     
     wp_enqueue_style("normalize",  get_template_directory_uri() . "/assets/css/normalize.css");
     wp_enqueue_style("webflow",  get_template_directory_uri() . "/assets/css/webflow.css");
-    wp_enqueue_style("devandplay",  get_template_directory_uri() ."/assets/css/devandplay.webflow.css");
-    wp_enqueue_style("devandplay-style", get_stylesheet_uri());
+    wp_enqueue_style("gamefeelideas",  get_template_directory_uri() ."/assets/css/gamefeelideas.webflow.css");
+    wp_enqueue_style("gamefeelideas-style", get_stylesheet_uri());
     
 }
-add_action("wp_enqueue_scripts", "devandplay_enqueue_styles");
-function devandplay_menu_setup(){
+add_action("wp_enqueue_scripts", "gamefeelideas_enqueue_styles");
+function gamefeelideas_menu_setup(){
     
     //Registers custom primary nav menu 
     register_nav_menus( array(
-        'logged-in'     => __("Logged-In Menu", "devandplay-login"),
-        'logged-out'    => __("Logged-Out Menu", "devandplay-logout"),
-        'main-nav'    => __("Main-Nav Menu", "devandplay-mainnav"),
+        'logged-in'     => __("Logged-In Menu", "gamefeelideas-login"),
+        'logged-out'    => __("Logged-Out Menu", "gamefeelideas-logout"),
+        'main-nav'    => __("Main-Nav Menu", "gamefeelideas-mainnav"),
         // 'drop-down'    => __("Dropdown Menu", "thegameclass-drop"),
         // 'second-nav'    => __("Second Nav Menu", "thegameclass-secondnav"),
         
     ));
     
 }
-add_action("after_setup_theme", "devandplay_menu_setup");
-function devandplay_setup(){
+add_action("after_setup_theme", "gamefeelideas_menu_setup");
+function gamefeelideas_setup(){
 	add_theme_support( 'post-formats', array(
 		'aside',
 		'image',
@@ -37,23 +37,23 @@ function devandplay_setup(){
 	
 	) );
 }
-add_action( 'after_setup_theme', 'devandplay_setup' );
+add_action( 'after_setup_theme', 'gamefeelideas_setup' );
 // remove_filter('the_content', 'wpautop');
 add_post_type_support( 'page', 'excerpt' );
 
-// add_filter ('get_the_excerpt','devandplay_filter_excerpt');
+// add_filter ('get_the_excerpt','gamefeelideas_filter_excerpt');
 
-// function devandplay_filter_excerpt ($post_excerpt) { 
+// function gamefeelideas_filter_excerpt ($post_excerpt) { 
 //   $post_excerpt = '<p class="jer_blog_sub">' . $post_excerpt . '</p>';
 //   return $post_excerpt;
 // }  
 
-function devandplay_widgets(){
+function gamefeelideas_widgets(){
   
     register_sidebar(array(
-     'name'           => __('Categories- Sidebar', 'devandplay'),
+     'name'           => __('Categories- Sidebar', 'gamefeelideas'),
      'id'             => 'article-sidebar',
-     'description'    => __('Add widgets here to appear in the sidebar', 'devandplay'),
+     'description'    => __('Add widgets here to appear in the sidebar', 'gamefeelideas'),
      'before_widget'  => '<div id="%1$s" class="%2$s">',
      'after_widget'   => '</div>',
      'before_title'   => '<h3 class="jer_h3"> ',
@@ -64,10 +64,10 @@ function devandplay_widgets(){
   
     
 }
-add_action('widgets_init', 'devandplay_widgets');
+add_action('widgets_init', 'gamefeelideas_widgets');
 
-add_filter('widget_title','devandplay_widget_title'); 
-function devandplay_widget_title($t)
+add_filter('widget_title','gamefeelideas_widget_title'); 
+function gamefeelideas_widget_title($t)
 {
 
     return null;

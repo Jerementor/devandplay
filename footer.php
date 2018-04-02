9b107055d92220f91037e314389e1615e7d40903
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-02 21:08:02 +0000

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<div class="dap_footer">
+<div class="w-container">
+  <div class="gfi_center">
+    <div>Copyright &copy; 2018, Game Feel Ideas</div>
+  </div>
+</div>
+</div>
 
 <?php wp_footer(); ?>
 

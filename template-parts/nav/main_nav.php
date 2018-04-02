5b0cb23a290e97fb3c8a906db574a9450f578d6c
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-02 20:11:20 +0000

    main nav change

--- template-parts/nav/main_nav.php
+++ template-parts/nav/main_nav.php
@@ -1,7 +1,7 @@
 <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
   <div class="w-container">
     <a href="/" class="w-nav-brand">
-      <h3 class="dap_brand">Dev <em>and</em> Play</h3>
+      <h3 class="dap_brand">Game Feel Ideas</h3>
     </a>
     <nav role="navigation" class="w-nav-menu">
       <?php get_template_part( 'template-parts/nav/main_nav_content'); ?>
